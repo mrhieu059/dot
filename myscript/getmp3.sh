f366b033a3836c4821d62ac3354cf2b271d8bfbb
Cinux <hieucao@protonmail.com>
2020-11-08 08:28:30 -0500

    Last update from artix linux

--- myscript/getmp3.sh
+++ myscript/getmp3.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/local/bin/bash
 #Download youtube via tridactly extension
 #Depencies: ffmpeg, youtube-dl, xclip and tridactly
 get='youtube-dl'
@@ -6,15 +6,7 @@ clip="$(xclip -sel clipboard -o)"
 get_Title=$(youtube-dl --get-filename $clip)
 
 #Ask user to confirm if they want to download this file
-user_Confirm="$(echo "Do you want to download this file? $get_Title" | dmenu)"
-case $user_Confirm in
-	yes)
-		notify-send "Downloading $get_Title" && $get $clip && notify-send "Finished" || notify-send "Failed to Download"
-		;;
-	no)
-		notify-send "Cancelling"
-		break
-		;;
-esac
+
+notify-send "Downloading $get_Title" && $get $clip && notify-send "Finished" || notify-send "Failed to Download"
 
 #notify-send "Downloading $get_Title " && $get $clip && notify-send "Finished" || notify-send "Failed to Download"
